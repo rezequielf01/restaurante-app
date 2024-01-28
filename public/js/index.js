8e28bf0d9ee7a86847dcff31e4b494d96c069794
rezequielf01 <rezequielf01@gmail.com>
2024-01-28 19:08:56 -0300

    responsive terminado

--- public/js/index.js
+++ public/js/index.js
@@ -51,9 +51,27 @@ $('.food-carousel').slick({
     dots: true,
     infinite: false,
     speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToShow: 6,
+    slidesToScroll: 6,
     responsive: [
+      {
+        breakpoint: 1921,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 5,
+          infinite: false,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 1441,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          infinite: false,
+          dots: true
+        }
+      },
       {
         breakpoint: 1025,
         settings: {
